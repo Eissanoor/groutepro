e83ca0fd3b5e389e13bc7f851ce5786a381923cd
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-07-31 21:10:35 +0500

    asdasdwqd

--- router/router.js
+++ router/router.js
@@ -353,5 +353,5 @@ router.delete("/SalesOrderDetsViewInvoiceLine_DELETE_BYID/:tblRouteMasterID",FAT
 router.delete("/SalesOrderDetsViewInvoiceLine1_DELETE_BYID/:tblRouteMasterID",FATSDB.SalesOrderDetsViewInvoiceLine1_DELETE_BYID)
 router.delete("/SalesOrderfromERPM_DELETE_BYID/:tblSalesInvoiceMID", FATSDB.SalesOrderfromERPM_DELETE_BYID)
 router.delete("/SalesOrderM_DELETE_BYID/:tblSalesOrderID",FATSDB.SalesOrderM_DELETE_BYID)
-//----------------------------------------------------------------------------////
+//--------------------------------------------------------------------------------------------------------////
 export default router;
